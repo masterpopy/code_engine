2d167592178cefcf0dc8ad3386e127f80da9616c
masterpopy <ilovengo@qq.com>
2018-08-05 10:47:19 +0800

    修改代码

--- scripts/insert.py
+++ scripts/insert.py
@@ -18,9 +18,8 @@ for candidatePath in Paths:
 		PATH = candidatePath
 		break
 if PATH == "":
-	#PATH = 'E:\\GAME\\GBA\\devkitARM\\bin'
-	PATH ='D:\\ZXZ\\ZXZ\\entr\\Pokemon\\RSEFL\\Hack\\devkitARM\\bin'
-	if os.path.isdir(PATH) == False:
+	PATH = 'E:\\GAME\\GBA\\devkitARM\\bin'
+	if not os.path.isdir(PATH):
 		print('Devkit not found.')
 		sys.exit(1)
 		
