954a244e251042aea464204b06d43a8d36aad300
masterpopy <ilovengo@qq.com>
2018-08-05 10:48:16 +0800

    修改代码

--- scripts/build.py
+++ scripts/build.py
@@ -45,14 +45,14 @@ def run_command(cmd):
 
 
 def make_output_file(filename):
-    '''Return hash of filename to use as object filename'''
+    """Return hash of filename to use as object filename"""
     m = hashlib.md5()
     m.update(filename.encode())
     return os.path.join(BUILD, m.hexdigest() + '.o')
 
 
 def process_assembly(in_file):
-    '''Assemble'''
+    """Assemble"""
     out_file = make_output_file(in_file)
     print('Assembling %s' % in_file)
     cmd = [AS] + ASFLAGS + ['-c', in_file, '-o', out_file]
@@ -61,7 +61,7 @@ def process_assembly(in_file):
 
 
 def process_c(in_file):
-    '''Compile C'''
+    """Compile C"""
     out_file = make_output_file(in_file)
     print('Compiling %s' % in_file)
     cmd = [CC] + CFLAGS + ['-c', in_file, '-o', out_file]
@@ -70,7 +70,7 @@ def process_c(in_file):
 
 
 def link(objects):
-    '''Link objects into one binary'''
+    """Link objects into one binary"""
     linked = 'build/linked.o'
     cmd = [LD] + LDFLAGS + ['-o', linked] + list(objects)
     run_command(cmd)
@@ -83,7 +83,7 @@ def objcopy(binary):
 
 
 def run_glob(globstr, fn):
-    '''Glob recursively and run the processor function on each file in result'''
+    """Glob recursively and run the processor function on each file in result"""
     if sys.version_info > (3, 4):
         files = glob(os.path.join(SRC, globstr), recursive=True)
         return map(fn, files)
