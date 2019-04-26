53cdd0384938763a8b715dcf72e2575995c80447
masterpopy <ilovengo@qq.com>
2019-04-26 17:07:12 +0800

    一个简单的例子，懂的请自行删除

---
+++ src/abcd.cpp
@@ -0,0 +1,19 @@
+#include "cppdefines.h"
+
+struct exzample{
+    pokemon poke_sggg;
+
+    void test_function2(){
+        poke_sggg.moves[0] = 12;
+    }
+} exzample1;
+
+
+
+externc void test_function1(){
+    exzample1.test_function2();
+    exzample1.poke_sggg.current_hp=0;
+    exzample1.poke_sggg.level = 0;
+
+    exzample1.poke_sggg.iv_hp=3;
+}
